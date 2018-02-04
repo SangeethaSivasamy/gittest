d74ecf3962a61fda3801f94c165f5d5ec686285d
sangee <sangeetha.sps@gmail.com>
2018-02-03 18:32:31 -0800

    commit to dev_branch

--- src/main/java/com/sang/git/Test1.java
+++ src/main/java/com/sang/git/Test1.java
@@ -5,6 +5,7 @@ public class Test1 {
 	public static void main(String[] args) {
 		System.out.println("Test1");
 		System.out.println("Testing git commands");
+		System.out.println("Testing dev_branch");
 	}
 	
 	public void print() {
