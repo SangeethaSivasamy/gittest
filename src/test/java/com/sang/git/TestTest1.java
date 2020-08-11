39967d8be848f4071516ffa04dd2dc7c90352a8d
SangeethaSivasamy <sangeetha.sps@gmail.com>
2020-08-10 21:53:09 -0700

    added two methods

--- src/test/java/com/sang/git/TestTest1.java
+++ src/test/java/com/sang/git/TestTest1.java
@@ -2,4 +2,11 @@ package com.sang.git;
 
 public class TestTest1 {
 
+	public void get1() {
+		System.out.println("This is test1");
+	}
+	
+	public void get2() {
+		System.out.println("This is test2");
+	}
 }
