16a46dd64a303b777a6bd4604d91c64016c5a9ef
SangeethaSivasamy <sangeetha.sps@gmail.com>
2020-08-11 10:46:14 -0700

    release-changed sysout to Test2

--- src/main/java/com/sang/git/Test1.java
+++ src/main/java/com/sang/git/Test1.java
@@ -3,7 +3,7 @@ package com.sang.git;
 public class Test1 {
 
 	public static void main(String[] args) {
-		System.out.println("Test1");
+		System.out.println("Test2");
 		System.out.println("Testing git commands");
 		System.out.println("Testing dev_branch");
 	
