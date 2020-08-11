9f7b78af5a5591f7402a57b997a98092b94b8d84
SangeethaSivasamy <sangeetha.sps@gmail.com>
2020-08-11 11:08:15 -0700

    release_branch changed Test2, added print1 method

--- src/main/java/com/sang/git/Test1.java
+++ src/main/java/com/sang/git/Test1.java
@@ -3,7 +3,7 @@ package com.sang.git;
 public class Test1 {
 
 	public static void main(String[] args) {
-		System.out.println("Test1");
+		System.out.println("Test2");
 		System.out.println("Testing git commands");
 		System.out.println("Testing dev_branch");
 	
@@ -12,6 +12,11 @@ public class Test1 {
 	}
 	
 	public void print() {
+		System.out.println("print");
+	}
+	
+	
+	public void print1() {
 		System.out.println("print");
 	}
 }
