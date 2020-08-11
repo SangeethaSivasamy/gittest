a5d70ad236de38f0e8693ce8ddf7c2523acec5f9
SangeethaSivasamy <sangeetha.sps@gmail.com>
2020-08-10 22:02:56 -0700

    dev

--- src/test/java/com/sang/git/TestTest1.java
+++ src/test/java/com/sang/git/TestTest1.java
@@ -3,10 +3,15 @@ package com.sang.git;
 public class TestTest1 {
 
 	public void get1() {
-		System.out.println("This is test1");
+		String str = "test";
+		System.out.println("This is test1. Make it complex");
 	}
 	
 	public void get2() {
+		System.out.println("This is test1. Make it still more complex");
+	}
+	
+	public void get3() {
 		System.out.println("This is test2");
 	}
 }
